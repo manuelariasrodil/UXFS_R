dff54b50899ff6683b1bf1dd4e7abf513f133667
m.arias1987@gmail.com <m.arias1987@gmail.com>
2013-03-12 19:28:21 +0100

    Fibonacci - 2 - Añadiendo caso de n = 45

--- FibonacciSeries.R
+++ FibonacciSeries.R
@@ -19,3 +19,7 @@ fib(10)
 # Test of function with n = 20
 fib(20)
 # [1]    1    1    2    3    5    8   13   21   34   55   89  144  233  377  610  987 1597 2584 4181 6765
+
+# Test of function with n = 45
+fib(45)
+
